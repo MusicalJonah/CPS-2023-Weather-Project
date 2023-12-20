2b2e97330278427403de0c971452f1cb66c455ab
MusicalJonah <120232584+MusicalJonah@users.noreply.github.com>
2023-12-20 00:02:41 +0000

    Added new templates and made minor changes.

--- api.py
+++ api.py
@@ -2,15 +2,42 @@ import requests
 import os
 import json
 
-def temperature(location):
+def temperature(zipCode):
+    apiKey = str(os.getenv("TOMORROW_IO_API_KEY"))
+    url = "https://api.tomorrow.io/v4/timelines?location="+ str(zipCode) +"&fields=temperature&timesteps=1h&units=metric&apikey="+ apiKey
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    print(url)
+    data = json.loads(response.text)
+    if 'data' in data:
+        temperature = data["data"]["values"]["temperature"]
+        return temperature
+    else:
+        return "Data not available"
+def dewPoint(location):
     apiKey = str(os.getenv("TOMORROW_IO_API_KEY"))
     url = "https://api.tomorrow.io/v4/weather/realtime?location="+ str(location) +"&apikey=" + apiKey
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
-    temperature = data["data"]["values"]["temperature"]
-    farenheit=temperature*9/5+32
-    return int(farenheit)
+    if 'data' in data:
+        dewPoint = data["data"]["values"]["dewPoint"]
+        return dewPoint
+    else:
+        return "Data not available"
+
+def windSpeed(location):
+    apiKey = str(os.getenv("TOMORROW_IO_API_KEY"))
+    url = "https://api.tomorrow.io/v4/weather/realtime?location="+ str(location) +"&apikey=" + apiKey
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    if 'data' in data:
+        kmph = data["data"]["values"]["windSpeed"]
+        mph=int(kmph)*0.621371
+        return int(mph)
+    else:
+        return "Data not available"
 
 def place(zipCode):
     apiKey = str(os.getenv("ZIPCODE_API_KEY"))
