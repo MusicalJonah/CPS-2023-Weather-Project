3e6d61d1751bcce6bab16c2860331bc37f15c196
MusicalJonah <120232584+MusicalJonah@users.noreply.github.com>
2023-12-21 22:43:28 +0000

    Removed dew point as new api doesn't support.

--- Server.py
+++ Server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/V1/<int:location>')
 def temperature(location):
-    return render_template('index.html', temperature=api.temperature(location), location=api.place(location), dewPoint=api.dewPoint(location), windSpeed=api.windSpeed(location))
+    return render_template('index.html', temperature=api.temperature(location), location=api.place(location), windSpeed=api.windSpeed(location))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
