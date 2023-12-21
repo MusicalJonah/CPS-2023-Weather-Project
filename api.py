762df7fbe0f038a973339cb525afac889936dd2d
MusicalJonah <120232584+MusicalJonah@users.noreply.github.com>
2023-12-21 22:42:13 +0000

    Moved over to weatherapi.com. Removed need for zipcodebase.com.

--- api.py
+++ api.py
@@ -2,50 +2,39 @@ import requests
 import os
 import json
 
+ApiKey = str(os.getenv("WeatherApiKey"))
+headers = {"accept": "application/json"}
+
 def temperature(zipCode):
-    apiKey = str(os.getenv("TOMORROW_IO_API_KEY"))
-    url = "https://api.tomorrow.io/v4/timelines?location="+ str(zipCode) +"&fields=temperature&timesteps=1h&units=metric&apikey="+ apiKey
-    headers = {"accept": "application/json"}
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(zipCode) +"&aqi=no"
     response = requests.get(url, headers=headers)
-    print(url)
     data = json.loads(response.text)
-    if 'data' in data:
-        temperature = data["data"]["values"]["temperature"]
+    print (data)
+    if 'current' in data:
+        temperature = data["current"]["temp_f"]
         return temperature
     else:
         return "Data not available"
-def dewPoint(location):
-    apiKey = str(os.getenv("TOMORROW_IO_API_KEY"))
-    url = "https://api.tomorrow.io/v4/weather/realtime?location="+ str(location) +"&apikey=" + apiKey
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    data = json.loads(response.text)
-    if 'data' in data:
-        dewPoint = data["data"]["values"]["dewPoint"]
-        return dewPoint
-    else:
-        return "Data not available"
 
-def windSpeed(location):
-    apiKey = str(os.getenv("TOMORROW_IO_API_KEY"))
-    url = "https://api.tomorrow.io/v4/weather/realtime?location="+ str(location) +"&apikey=" + apiKey
-    headers = {"accept": "application/json"}
+
+def windSpeed(zipCode):
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(zipCode) +"&aqi=no"
     response = requests.get(url, headers=headers)
     data = json.loads(response.text)
-    if 'data' in data:
-        kmph = data["data"]["values"]["windSpeed"]
-        mph=int(kmph)*0.621371
+    if 'current' in data:
+        mph = data["current"]["wind_mph"]
         return int(mph)
     else:
         return "Data not available"
-
-def place(zipCode):
-    apiKey = str(os.getenv("ZIPCODE_API_KEY"))
-    url = "https://app.zipcodebase.com/api/v1/search?apikey="+ apiKey +"&codes="+ str(zipCode) +"&country=US"
-    headers = {"accept": "application/json"}
+def place(location):
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(location) +"&aqi=no"
     response = requests.get(url, headers=headers)
-    print(url)
     data = json.loads(response.text)
-    city = data["results"][str(zipCode)][0]["city"]
-    state = data["results"][str(zipCode)][0]["state"]
-    return (city + ", " + state)
+    if 'location' in data:
+        town = data["location"]["name"]
+        state = data["location"]["region"]
+        country = data["location"]["country"]
+        place = town + ", " + state + ", " + country
+        return place
+    else:
+        return "Data not available"
