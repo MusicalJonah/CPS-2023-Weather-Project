94469d72774eedc263f85da8c2d3d6519724aeed
MusicalJonah <120232584+MusicalJonah@users.noreply.github.com>
2023-12-23 18:38:57 +0000

    Changed template to use bootstrap

--- server.py
+++ server.py
@@ -7,14 +7,9 @@ def weather(location):
     temperature, windSpeed, pressure, humidity, condition, image, location = api.weather(location)
     return render_template('index.html', temperature=temperature, windSpeed=windSpeed, pressure=pressure, humidity=humidity, condition=condition, image=image, location=location)
 
-@app.route('/style.css')
-def styles():
-    return render_template('style.css')
-
-@app.route('/index.css')
-def index():
-    return render_template('index.css')
-
+@app.route('/favicon.ico')
+def favicon():
+    return render_template('favicon.ico')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
