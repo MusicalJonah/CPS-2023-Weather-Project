91ee0f42a7fbb97bc247f3515507cb83d0efc83c
MusicalJonah <120232584+MusicalJonah@users.noreply.github.com>
2023-12-23 17:28:15 +0000

    Made some changes to template. New bootstrap version coming soon.

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from api import weather
+import api
 app = Flask(__name__)
 
 @app.route('/<int:location>')
 def weather(location):
-    temperature, windSpeed, pressure, humidity, condition, image, location = weather(location)
+    temperature, windSpeed, pressure, humidity, condition, image, location = api.weather(location)
     return render_template('index.html', temperature=temperature, windSpeed=windSpeed, pressure=pressure, humidity=humidity, condition=condition, image=image, location=location)
 
 @app.route('/style.css')
