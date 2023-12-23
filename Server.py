54e0dc906ca81c7c92ed222a48fe84f3c96972e9
MusicalJonah <120232584+MusicalJonah@users.noreply.github.com>
2023-12-23 01:56:16 +0000

    Made changes to include new template.

--- Server.py
+++ Server.py
@@ -3,9 +3,18 @@ import api
 
 app = Flask(__name__)
 
-@app.route('/V1/<int:location>')
+@app.route('/<int:location>')
 def temperature(location):
-    return render_template('index.html', temperature=api.temperature(location), location=api.place(location), windSpeed=api.windSpeed(location))
+    return render_template('index.html', temperature=api.temperature(location), location=api.place(location), windSpeed=api.windSpeed(location), pressure=api.pressure(location), humidity=api.humidity(location), condition=api.condition(location), image=api.image(location))
+
+@app.route('/style.css')
+def styles():
+    return render_template('style.css')
+
+@app.route('/index.css')
+def index():
+    return render_template('index.css')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- api.py
+++ api.py
@@ -26,6 +26,46 @@ def windSpeed(zipCode):
         return int(mph)
     else:
         return "Data not available"
+    
+def pressure(location):
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(location) +"&aqi=no"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    if 'current' in data:
+        pressure = data["current"]["pressure_mb"]
+        return pressure
+    else:
+        return "Data not available"
+def humidity(location):
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(location) +"&aqi=no"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    if 'current' in data:
+        humidity = data["current"]["humidity"]
+        return humidity
+    else:
+        return "Data not available"
+    
+def condition(location):
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(location) +"&aqi=no"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    if 'current' in data:
+        condition = data["current"]["condition"]["text"]
+        return condition
+    else:
+        return "Data not available"
+
+def image(location):
+    url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(location) +"&aqi=no"
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    if 'current' in data:
+        image = data["current"]["condition"]["icon"]
+        return ("https://" + image)
+    else:
+        return "Data not available"
+
 def place(location):
     url = "https://api.weatherapi.com/v1/current.json?key="+ ApiKey +"&q="+ str(location) +"&aqi=no"
     response = requests.get(url, headers=headers)
